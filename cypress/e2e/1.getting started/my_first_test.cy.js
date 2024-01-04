1102942cc403f1c99ef714d6e469274a5e96d787
fazleyazdan <fazleyazdan345@gmail.com>
2024-01-04 17:08:55 +0500

    getting started with cypress

---
+++ cypress/e2e/1.getting started/my_first_test.cy.js
@@ -0,0 +1,20 @@
+// describe represent test suit . replace 'suit name' with your desired suit name
+
+describe('suit name', () => {
+
+  it('my test case', () => {
+    expect(true).to.equal(true)
+  })
+  
+})
+
+  // we can replace arrow with the name function
+
+  // describe('suit name', () => {
+
+  //   it('Does not do much!', function() {
+  //     expect(true).to.equal(true)
+  //   })
+
+  // })
+
---
+++ cypress/e2e/1.getting started/test_suit.cy.js
@@ -0,0 +1,14 @@
+// There can be multiple test cases inside a test suit.
+
+
+describe('suit name', () => {
+
+    it('test case 1', () => {
+      expect(true).to.equal(true)
+    })
+
+    it('test case 2', () => {
+        expect(true).to.equal(false)
+      })
+
+  })
