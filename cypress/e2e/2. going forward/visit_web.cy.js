19da813020a7ddb35b1ab8597a1731498e3ffe43
fazleyazdan <fazleyazdan345@gmail.com>
2024-01-05 16:57:24 +0500

    adding comment

--- cypress/e2e/2. going forward/visit_web.cy.js
+++ cypress/e2e/2. going forward/visit_web.cy.js
@@ -2,8 +2,8 @@
 describe('OrangeHRM Automation', () =>{
 
     it('verify title Positive-test', () => {
-        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        cy.title().should('eq', 'OrangeHRM')
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")    /* cy.visit for visiting web*/
+        cy.title().should('eq', 'OrangeHRM')                                              /* cy.title() captures title of a web */
     })
 
     it('verify title Negative-test', () => {
