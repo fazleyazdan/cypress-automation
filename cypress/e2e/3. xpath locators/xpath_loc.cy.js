a77365f58ef0969def2ba9318b0d9c534f56019b
fazleyazdan <fazleyazdan345@gmail.com>
2024-01-10 17:08:06 +0500

    chained xpath

--- cypress/e2e/3. xpath locators/xpath_loc.cy.js
+++ cypress/e2e/3. xpath locators/xpath_loc.cy.js
@@ -9,4 +9,12 @@ describe("Practcing xpath", () => {
         cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
         cy.xpath("//img").should('have.length', 1)                    
     })
+
+    it("chained xpath", () => {
+
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+        // "//tbody/tr/td[1]/ul[1]/li[1]" full xpath
+        cy.xpath("//tbody/tr/td[1]/ul[1]").xpath("./li[1]").should('have.length', 1)                    
+    })
 })
