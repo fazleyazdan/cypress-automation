cb958c5a56bf93cb1e4b3a075defe8fa4dd7a7be
fazleyazdan <fazleyazdan345@gmail.com>
2024-01-24 17:29:27 +0500

    working on checkboxes

---
+++ cypress/e2e/4. radioBtn_&_checkbox/checkboxes.cy.js
@@ -0,0 +1,12 @@
+
+
+
+describe('testing UI Elements', () => {
+
+    it('testing checkboxes', () => {
+      
+        cy.visit("https://testautomationpractice.blogspot.com/")
+
+    })
+    
+  })
