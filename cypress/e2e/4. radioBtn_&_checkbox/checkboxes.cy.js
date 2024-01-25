5a08a18cad7e50cc21769d9deced1484e564d232
fazleyazdan <fazleyazdan345@gmail.com>
2024-01-25 13:24:38 +0500

    handling checkboxes

--- cypress/e2e/4. radioBtn_&_checkbox/checkboxes.cy.js
+++ cypress/e2e/4. radioBtn_&_checkbox/checkboxes.cy.js
@@ -6,6 +6,27 @@ describe('testing UI Elements', () => {
     it('testing checkboxes', () => {
       
         cy.visit("https://testautomationpractice.blogspot.com/")
+        
+        // verifying if the checkbox is visible
+        cy.get("#sunday").should('be.visible')
+
+        // checking and uncheking single checkbox and applying assertion
+        cy.get("#sunday").check().should('be.checked')
+        cy.get("#sunday").uncheck().should('not.be.checked')
+
+        // checking/selecting all the checkboxes at one time: [for that we need to write a common xpath or css selector]
+        cy.xpath("//input[contains(@value,'day')]").check().should('be.checked')
+
+        // unchecking/unselecting all the checkboxes at one time
+        cy.xpath("//input[contains(@value,'day')]").uncheck().should('not.be.checked')
+
+        // Selecting first or last checkbox among multiple checkboxes
+        cy.xpath("//input[contains(@value,'day')]").first().check()
+        cy.xpath("//input[contains(@value,'day')]").last().check().should("be.checked")
+
+
+        
+
 
     })
     
