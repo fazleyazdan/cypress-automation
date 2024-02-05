a8ba7526d815cc612b64bc5bdd30597dc347bf75
fazleyazdan <fazleyazdan345@gmail.com>
2024-02-05 19:17:26 +0500

    child tabs completed

--- cypress/e2e/7. Handling tabs/Readme/For understanding/readme.md
+++ cypress/e2e/7. Handling tabs/Readme/For understanding/readme.md
@@ -1,11 +1,21 @@
 #### Handling Child Tabs In Cypress :
 
-**By Default cypress does not handle child tabs**
+**By Default cypress does not handle child tabs/windows**
 
 *When you are clicking on a text and it leads you to a child tab or new window, an anchor tag is used for that.* **see child_tabs.png** in screenshots folder
 
+#### Approach 1:
 an anchor tag has an attribute called **target**. and it has a value *_blank* for opening the tab in new window. **see child_tabs_2.png** in screenshots folder.
 
 so to open the tab in same window we will remove the **target** attribute.
 
-** we can remove that attribute by using **envoke** methode.
+** we can remove that attribute by using **invoke** methode.
+
+#### Approach 2:
+*we know that an anchor tag has href attribute, in which we specify the url of the target window/tab.*
+
+Now in this approach we will capture the value of **href** and then we will use another **cy.visit** to visit that page.
+
+**this approach has a limitation. the domain of the child and parent should be the same. otherwise the child tab won't be accessed**
+This is the domain *'https://the-internet.herokuapp.com'*. it must be same.
+sub domain can be anything. ie. '/windows' or 'window/new'.
--- cypress/e2e/7. Handling tabs/child_tabs.cy.js
+++ cypress/e2e/7. Handling tabs/child_tabs.cy.js
@@ -1,7 +1,7 @@
 //* Handling Child Tabs
 //! Note : read 'readme' before starting this
 
-describe('Handle Tab Approach 1', () =>{
+describe('Handle Tabs', () =>{
 
     it('Approach 1', () =>{
 
@@ -15,7 +15,32 @@ describe('Handle Tab Approach 1', () =>{
         // lets suppose you have done some operation on child tab and wanna go back to parent tab
         cy.go('back')
 
-       
+    })
+
+    //!  Approach 2:
+    //* we know that an anchor tag has 'href' attribute, in which we specify the url of the target window/tab.
+    //* Now in this approach we will capture the value of 'href' and then we will use another 'cy.visit' to visit that page.
+    //! this approach has a limitation. the domain of the child and parent should be the same. otherwise the child tab won't be accessed
+    //* this is the domain 'https://the-internet.herokuapp.com'. it must be same.
+    //* sub domain can be anything. ie. '/windows' or 'window/new'.
+
+    it.only('Approach 2', () =>{
+
+        cy.visit('https://the-internet.herokuapp.com/windows')         
+        
+        // we will use 'Jquery' Function to capture 'href'. 
+
+        cy.get(".example > a").then((e) => {
+            let url = e.prop('href')                     //! "e.prop" capture the property. which is 'href' in this case.
+            cy.visit(url)    
+        })
+
+        //* now lets validate the url. if we have accessed the child tab or not
+        cy.url().should('include', 'https://the-internet.herokuapp.com/windows/new')
+
+        cy.wait(3000)
+        cy.go('back')
+
     })
 
 })   
