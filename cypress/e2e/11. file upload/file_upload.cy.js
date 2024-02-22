73921f3129c5867aca03ed5b0f90e12987e9e86f
fazleyazdan <fazleyazdan345@gmail.com>
2024-02-22 12:29:35 +0500

    single file upload

--- cypress/e2e/11. file upload/file_upload.cy.js
+++ cypress/e2e/11. file upload/file_upload.cy.js
@@ -1,10 +1,33 @@
 // first install cypress file upload plugin. [npm install cypress --save-dev cypress-file-upload]
+//! Note : keep the file in fixture folder, that you want to upload 
 
 describe('handling file upload', ()=> {
 
     it('single file uplaod', ()=> {
 
         cy.visit("https://the-internet.herokuapp.com/upload")
-        
+        cy.get("#file-upload").attachFile('dummy-1.pdf')
+        cy.get("#file-submit").click()
+        cy.wait(3000)
+        // after uploading a file , 'file uploaded' message is displayed. put assertion on it.
+        cy.get("div[class='example'] h3").should('have.text', 'File Uploaded!')
+
     })
+
+    it.only('single file uplaod - rename file when uploading', ()=> {
+        //* change the name of file at the time of upload. 
+        //* filePath refers to the actual file in fixture folder.
+        //* fileName used for changing the file name. 
+
+        cy.visit("https://the-internet.herokuapp.com/upload")
+        cy.get("#file-upload").attachFile({filePath:'dummy-1.pdf', fileName: 'namechanged.pdf'})
+        cy.get("#file-submit").click()
+        cy.wait(3000)
+        // after uploading a file , 'file uploaded' message is displayed. put assertion on it.
+        cy.get("div[class='example'] h3").should('have.text', 'File Uploaded!')
+
+    })
+
+    
 })
+
