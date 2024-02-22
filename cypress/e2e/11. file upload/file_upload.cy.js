dd3b8e7cfcf6ec60b9454e7073dfa6273b9c8cb2
fazleyazdan <fazleyazdan345@gmail.com>
2024-02-22 14:33:20 +0500

    file upload completed

--- cypress/e2e/11. file upload/file_upload.cy.js
+++ cypress/e2e/11. file upload/file_upload.cy.js
@@ -6,15 +6,15 @@ describe('handling file upload', ()=> {
     it('single file uplaod', ()=> {
 
         cy.visit("https://the-internet.herokuapp.com/upload")
-        cy.get("#file-upload").attachFile('dummy-1.pdf')
+        cy.get("#file-upload").attachFile('dummy-1.pdf')     //! '#file-upload' locator of choose file   
         cy.get("#file-submit").click()
         cy.wait(3000)
-        // after uploading a file , 'file uploaded' message is displayed. put assertion on it.
+        //* after uploading a file , 'file uploaded' message is displayed. put assertion on it.
         cy.get("div[class='example'] h3").should('have.text', 'File Uploaded!')
 
     })
 
-    it.only('single file uplaod - rename file when uploading', ()=> {
+    it('single file uplaod - rename file when uploading', ()=> {
         //* change the name of file at the time of upload. 
         //* filePath refers to the actual file in fixture folder.
         //* fileName used for changing the file name. 
@@ -28,6 +28,34 @@ describe('handling file upload', ()=> {
 
     })
 
-    
+    it('single file upload - drag & drop', ()=> {
+
+        cy.visit("https://the-internet.herokuapp.com/upload")
+        cy.get("#drag-drop-upload").attachFile('dummy-1.pdf', {subjectType: 'drag-n-drop'})
+        cy.wait(2000)
+        //* assertion on the name of the uploaded file 
+        cy.get('#drag-drop-upload > .dz-preview > .dz-details > .dz-filename > span').contains( 'dummy-1.pdf')
+    })
+
+    it('multiple file upload', ()=> {
+
+        cy.visit("https://davidwalsh.name/demo/multiple-file-upload.php")
+        
+        //* if more than one file, we use list for that purpose. 
+        cy.get("#filesToUpload").attachFile(['dummy-1.pdf','dummy-2.pdf'])
+        cy.wait(2000)
+        
+    })
+
+    it.only('file upload - shadow DOM', ()=> {
+        //! shadow dom also called sub dom. it cannot be accessed by XPATH locators, use css selectors
+        //* we will add a parameter in cy.get with the locator in order to interact with it.
+
+        cy.visit("https://www.htmlelements.com/demos/fileupload/shadow-dom/index.htm")
+        cy.get(".smart-browse-input", {includeShadowDom:true}).attachFile('dummy-2.pdf')
+        cy.wait(2000)
+        cy.get(".smart-item-name", {includeShadowDom:true}).should('have.text','dummy-2.pdf')
+        
+    })
 })
 
