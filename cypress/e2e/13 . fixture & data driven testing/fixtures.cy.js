f7cc9ee5741c94fc387a97e61b7ed8af7713d9d6
fazleyazdan <fazleyazdan345@gmail.com>
2024-02-23 17:15:05 +0500

    fixtures task 1 completed

---
+++ cypress/e2e/13 . fixture & data driven testing/fixtures.cy.js
@@ -0,0 +1,37 @@
+// in cypress, fixture is a folder in which we add test data files which can be used for testing
+// it can be an image, an excel sheet or json file which contains some data in json format.
+// for this task i have created a json file named 'practicetest.json' in fixture folder.
+// this file contains login data for the 'practicetestautomation' website.
+
+
+describe('fixtures test suit', () => {
+
+    it('without using fixture', () => {
+
+        cy.visit("https://practicetestautomation.com/practice-test-login/")   
+        cy.get("#username").type("student")
+        cy.get("#password").type("Password123")
+        cy.get("#submit").click()
+        cy.wait(2000)
+        cy.get(".post-title").should('have.text', 'Logged In Successfully')
+        cy.log("*** test case passed ***")
+
+    })
+
+    it.only('fixture Demo', () => {
+        
+        cy.visit("https://practicetestautomation.com/practice-test-login/")   
+        cy.fixture('practicetest').then( (data) => {
+            cy.get("#username").type(data.username)
+            cy.get("#password").type(data.password)
+            cy.get("#submit").click()
+            cy.wait(2000)
+            cy.get(".post-title").should('have.text', data.expected)
+        })
+        cy.log("*** test case passed ***") 
+        
+    })
+
+  })
+
+
