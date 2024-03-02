65d74f9d3bef7a751f33c707ddea3a8e1d436c05
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-02 22:13:18 +0500

    custom commands started

---
+++ cypress/e2e/14. custom commands/custom_commands.cy.js
@@ -0,0 +1,17 @@
+//! sometimes in cypress you have to write custom commands for performing certain tasks
+//* we write custom commands in 'commands.js' in support folder
+
+describe('custom commands suit', () => {
+
+    //* in selenium we can find a link through 'link text' or label. 
+    //* but in cypress we can't do it. to do that we can write custom command for it.
+
+    it('handling links', () => {
+     
+    })
+
+    it('test case 2', () => {
+       
+      })
+
+  })
