8e86b7a3fd1c927276a97a4be7a9ebc788eadd03
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-05 11:45:34 +0500

    browser navigation completed

--- cypress/e2e/15. browser navigation/browser_nav.cy.js
+++ cypress/e2e/15. browser navigation/browser_nav.cy.js
@@ -8,7 +8,25 @@ describe('Browser Navigations test suit', () => {
     
         cy.visit("https://practice-automation.com/")
         cy.title().should('eq', 'Home | Practice Automation')   //* verify title of home page that we are on h.page
-        cy.clickLink('JavaScript Delays')
+        
+        // cy.get("#javascriptdelay").click()
+        cy.clickLink('JavaScript Delays')                   //* used custom commands to click and go to the other page
+        cy.title().should('eq', 'JavaScript Delays | Practice Automation')   //* verify title of the other page
+        cy.go('back')                       //* go back to home page
+        
+        cy.title().should('eq', 'Home | Practice Automation')   //* verify title of home page that we are on h.page
+        cy.go('forward')
+        cy.title().should('eq', 'JavaScript Delays | Practice Automation')   //* verify title of the other page
+        
+        cy.reload()
+
+        //! alternative to 'back' & 'forward'
+        cy.go(-1)                       //* go back to home page
+        cy.title().should('eq', 'Home | Practice Automation')   //* verify title of home page that we are on h.page
+        cy.go(1)                        //* go forward
+        cy.title().should('eq', 'JavaScript Delays | Practice Automation')   //* verify title of the other page
+        
+        cy.reload()
     })
   
 })
