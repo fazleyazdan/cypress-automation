22faa45409cddd169c2c1a21f214a63543c4dacd
fazleyazdan <fazleyazdan345@gmail.com>
2024-03-05 17:50:09 +0500

    html report generator completed

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  video:true,
+  // video:true,
+  reporter: 'cypress-mochawesome-reporter',         // for generating html reports
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on)
     },
   },
 });
---
+++ cypress/e2e/17. generate html reports/reports.cy.js
@@ -0,0 +1,9 @@
+//* in cypress we can generate html report by installing a package called 'cypress-mochawesome-reporter'
+//* after installing it we have to do some changes in 'cypress.config.js'. see package info on npm website
+//* add this: import 'cypress-mochawesome-reporter/register' in e2e.js.
+
+//* run any testsuit/test cases on headless or headed mode and the report will be generated for that test cases.
+//* to run it in the headed mode write this statement
+//* npx cypress run --headed --spec "relative path of test"
+//! the report will be saved in the report folder inside index.html
+//* right click on it and copy full of path of index.html. then paste that path in chrome and hit enter
